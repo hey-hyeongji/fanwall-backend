00fea3593c33942c98b5f00d4cfa8a77baac8c89
hey-hyeongji <julianrankit@gmail.com>
2025-04-21 23:18:42 +0900

    Fix require path: change Change.js to change.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Change = require('./models/Change');
+const Change = require('./models/change');
 
 const app = express();
 app.use(cors());
