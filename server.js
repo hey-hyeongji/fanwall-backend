3bdc89402bcec5b4648a21f19d12e745b936d6bc
hey-hyeongji <julianrankit@gmail.com>
2025-04-23 00:03:17 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const Change = require('./models/change');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
